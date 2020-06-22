df52965269f1aee7407d285bb2ea1f1b97c7bfab
Michael Hallett <m.hallett@gmail.com>
2020-06-22 20:31:12 +0000

    Turn mechanics definition

--- data/turn.rb
+++ data/turn.rb
@@ -1,26 +1,28 @@
 class Turn
-  @@turn_number = 0
 
-  def initialize
-    @@turn_number += 1
-    @turn = @@turn_number
-  end
-  
-  def current_turn
-    puts "Turn #{@turn}"
+  attr_reader :player1, :player2, :current_player
+
+  def initialize (player1, player2, current_player)
+    @player1 = player1
+    @player2 = player2
+    @current_player = current_player
   end
 
-  def validate_answer(question_sum, player_answer, player)
+  def new_turn
+    puts "---NEW TURN---"
+  end
+
+  def validate_answer(question_sum, player_answer)
     if question_sum == player_answer
-      puts "#{player.name}: YES! you are correct!"
+      puts "#{@current_player.name}: YES! you are correct!"
     else
-      puts "#{player.name}: Seriously? NO!"
-      player.decrease_life
+      puts "#{@current_player.name}: Seriously? NO!"
+      current_player.decrease_life
     end
 
   end
 
-  def player_scores(player1, player2)
-    puts "P1: #{player1.current_life} vs P2: #{player2.current_life}"
+  def player_scores
+    puts "P1: #{@player1.current_life} vs P2: #{@player2.current_life}"
   end
 end
